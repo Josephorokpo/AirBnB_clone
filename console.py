91542a70791c9b8e99eae7fc3f6f653d91664cd8
josephorokpo <josephorokpo@gmail.com>
2023-12-08 02:56:29 -0800

    Update console to allow actons for all classes

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ Module for the command interpreter.
 
 import cmd
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 
